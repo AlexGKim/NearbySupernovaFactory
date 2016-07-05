69c4f3d3d45136af9e1ae3f35fc9921772836957
Alex Kim <akim@40.51.243.131.in-addr.dhcp.lbl.gov>
2016-07-05 15:53:28 -0700

    refining second model

--- gerard2.stan
+++ gerard2.stan
@@ -1,29 +1,29 @@
 data {
   int D;                // Number of supernovae
-  int N_colors;
+  int N_mags;
   int N_EWs;
-  vector[N_colors] color_obs[D];
+  vector[N_mags] mag_obs[D];
   vector[N_EWs] EW_obs[D];
-  matrix[N_colors, N_colors] color_cov[D];
+  matrix[N_mags, N_mags] mag_cov[D];
   matrix[N_EWs, N_EWs] EW_cov[D];
 
-  real color_std;
-  vector[N_colors] color_mn;
+  # real mag_std;
+  # vector[N_mags] mag_mn;
 }
 
 parameters {
 
-  vector<lower=-5, upper=5>[2] EW[D];
+  vector<lower=-100, upper=100>[2] EW[D];
+  vector<lower=-2, upper=2>[N_mags] mag_int[D];
 
-  vector[N_colors] color_int[D];
-  vector<lower=-1, upper=1>[5] c;
-  vector<lower=-5, upper=5>[5] alpha;
-  vector<lower=-5, upper=5>[5] beta;
-  cholesky_factor_corr[N_colors] L_Omega;
-  vector<lower=0>[N_colors] L_sigma;
+  vector<lower=-0.3, upper=0.3>[5] c;
+  vector<lower=-0.05, upper=0.05>[5] alpha;
+  vector<lower=0.005, upper=0.1>[5] beta;
+  cholesky_factor_corr[N_mags] L_Omega;
+  vector<lower=0.02, upper = 0.2>[N_mags] L_sigma;
 
-  vector<lower=-5, upper=5>[4] gamma;
-  real<lower=-5, upper=5> k[D-1];
+  vector<lower=0.5, upper=1.8>[4] gamma;
+  real<lower=-3, upper=3> k[D-1];
 }
 
 model {
@@ -33,8 +33,8 @@ model {
   matrix[5,5] L_Sigma;
 
   gamma_[1]<- gamma[1];
-  gamma_[2] <- 1.;
-  gamma_[3] <- gamma[2];
+  gamma_[2] <- gamma[2];
+  gamma_[3] <- 1.;
   gamma_[4] <- gamma[3];
   gamma_[5] <- gamma[4];
 
@@ -47,26 +47,26 @@ model {
       means[d] <- c + alpha*EW[d,1]  + beta*EW[d,2];
   }
 
-  L_sigma ~ cauchy(0,2.5);
+  L_sigma ~ cauchy(0.1,2.5);
   L_Omega ~ lkj_corr_cholesky(2.);
   L_Sigma <- diag_pre_multiply(L_sigma, L_Omega);
 
   for (d in 1:(D-1)) {
-    color_int[d] ~ multi_normal_cholesky(means[d], L_Sigma);
-    color_obs[d] ~ multi_normal(means[d], color_cov[d]);
+    mag_int[d] ~ multi_normal_cholesky(means[d], L_Sigma);
+    mag_obs[d] ~ multi_normal(mag_int[d]+gamma_*k[d], mag_cov[d]);
     EW_obs[d] ~ multi_normal(EW[d], EW_cov[d]);
   }  
 }
 
-generated quantities {
-  # corr_matrix[4] Omega_color;
-  vector[5] c_unnorm;
-  vector[5] alpha_unnorm;
-  vector[5] beta_unnorm;
-  vector[4] gamma_unnorm;
-  # vector[3] gamma_unnorm;
-  c_unnorm <- c*color_std + color_mn;
-  alpha_unnorm <- alpha*color_std;
-  beta_unnorm <- beta*color_std;
-  gamma_unnorm <- gamma*color_std;
-}
+# generated quantities {
+#   # corr_matrix[4] Omega_mags;
+#   vector[5] c_unnorm;
+#   vector[5] alpha_unnorm;
+#   vector[5] beta_unnorm;
+#   vector[4] gamma_unnorm;
+#   # vector[3] gamma_unnorm;
+#   c_unnorm <- c*mags_std + mags_mn;
+#   alpha_unnorm <- alpha*mags_std;
+#   beta_unnorm <- beta*mags_std;
+#   gamma_unnorm <- gamma*mags_std;
+# }
--- view.py
+++ view.py
@@ -5,10 +5,11 @@ from matplotlib import rc
 import corner
 from matplotlib.backends.backend_pdf import PdfPages
 import numpy
+import sncosmo
 
 color_std=0.272560118531
 
-f = open('temp.pkl','rb')
+f = open('temp2.pkl','rb')
 fit = pickle.load(f)
 
 
@@ -19,10 +20,11 @@ with PdfPages('multipage_pdf.pdf') as pdf:
     # pdf.savefig()
     # plt.close()
 
-    # plt.plot(fit['L_sigma_EW'])
-    # plt.title('L_sigma_EW')
-    # pdf.savefig()
-    # plt.close()
+    lineobjects = plt.plot(fit['L_sigma'],label=[])
+    plt.title(r'$L_\sigma$')
+    plt.legend(iter(lineobjects),('U','B','V','R','I'))
+    pdf.savefig()
+    plt.close()
     
     plt.plot(fit['EW'][::10,:10,0])
     plt.title('EW - 0')
@@ -34,23 +36,28 @@ with PdfPages('multipage_pdf.pdf') as pdf:
     pdf.savefig()
     plt.close()
 
-    plt.plot(fit['c_unnorm'])
+    lineobjects = plt.plot(fit['c'],label=['U','B','V','R','I'])
     plt.title('c')
+    plt.legend(iter(lineobjects),('U','B','V','R','I'))
+
     pdf.savefig()
     plt.close()
     
-    plt.plot(fit['alpha'][::10],alpha=0.5)
+    lineobjects = plt.plot(fit['alpha'][::10],alpha=0.5,label=['U','B','V','R','I'])
     plt.title('alpha')
+    plt.legend(iter(lineobjects),('U','B','V','R','I'))
     pdf.savefig()
     plt.close()
     
-    plt.plot(fit['beta'][::10],alpha=0.5)
+    lineobjects = plt.plot(fit['beta'][::10],alpha=0.5,label=['U','B','V','R','I'])
     plt.title('beta')
+    plt.legend(iter(lineobjects),('U','B','V','R','I'))
     pdf.savefig()
     plt.close()
     
-    plt.plot(fit['gamma'])
+    lineobjects = plt.plot(fit['gamma'],label=['U','B','R','I'])
     plt.title('gamma')
+    plt.legend(iter(lineobjects),('U','B','R','I'))
     pdf.savefig()
     plt.close()
     
@@ -77,25 +84,48 @@ with PdfPages('multipage_pdf.pdf') as pdf:
     # plt.close()
 
     # filter wavelengths
-    edges = numpy.array([[3300, 4102], [4102, 5100], [5200, 6289], [6289, 7607], [7607, 9200]])
+    edges = numpy.array([[3300., 4102], [4102, 5100], [5200, 6289], [6289, 7607], [7607, 9200]])
     efflam = []
     for edge in edges:
         efflam.append((edge[1]-edge[0])/2+edge[0])
 # [3701, 4601, 5744, 6948, 8403]
     rc('text', usetex=True)
-    dif = numpy.array([1.545252680138557- 0.9512179494794494, 1.251613863650247- 0.9512179494794494, 0.9512179494794494-0.7582486105024363, 0.9512179494794494-0.5429015873250165]) 
-    dif = dif/dif[1]
 
-    dum = [dif[0], dif[2], dif[3]]
-    figure = corner.corner(fit['gamma'],labels=[r"${\gamma}_0$",r"${\gamma}_2$",r"${\gamma}_3$"],truths=dum)
+    lambdas = numpy.arange(3500.,9000,100)
+    rvs = [2.1,3.1,4.1]
+    for rv in rvs:
+        A_ = sncosmo._extinction.ccm89(lambdas, 1., rv)
+        norm  = sncosmo._extinction.ccm89(numpy.array([efflam[2]]), 1., rv)
+        A_ = A_/norm[0]
+        plt.plot(lambdas,A_,label=r"$R_V={:.1f}$".format(rv))
+
+    dum = (1-.68)/2
+    gamma_sort = numpy.sort(fit['gamma'],axis=0)
+    ngamma = gamma_sort.shape[0]
+    y = gamma_sort[ngamma/2,:]
+    ymin = gamma_sort[ngamma*dum,:]
+    ymax  = gamma_sort[ngamma*(1-dum),:]
+    plt.errorbar(numpy.delete(efflam,2),y,yerr=[y-ymin,ymax-y],fmt='o')
+    plt.legend()
+    pdf.savefig()
+    plt.close()
+    # dif = numpy.array([1.545252680138557- 0.9512179494794494, 1.251613863650247- 0.9512179494794494, 0.9512179494794494-0.7582486105024363, 0.9512179494794494-0.5429015873250165,0.9512179494794494])
+    # dif = dif/dif[1]
+
+    # dum = [dif[0], dif[2], dif[3], dif[4]]
+    figure = corner.corner(fit['gamma'],labels=[r"${\gamma}_0$",r"${\gamma}_1$",r"${\gamma}_3$",r"${\gamma}_4$"])
     pdf.savefig()
     plt.close()
 
-    for index in xrange(4):
-        dum = numpy.swapaxes(numpy.array([fit['alpha'][:,index]*color_std,fit['beta'][:,index]*color_std]),0,1)
-        figure = corner.corner(dum,labels=[r"$\alpha_{}$".format(index),r"$\beta_{}$".format(index)],truths=[0,0])
-        pdf.savefig()
-        plt.close()
+    figure = corner.corner(fit['L_sigma'],labels=[r"${L_\sigma}_0$",r"${L_\sigma}_1$",r"${L_\sigma}_2$",r"${L_\sigma}_3$",r"${L_\sigma}_4$"])
+    pdf.savefig()
+    plt.close()
+
+    # for index in xrange(5):
+    #     dum = numpy.swapaxes(numpy.array([fit['alpha'][:,index]*color_std,fit['beta'][:,index]*color_std]),0,1)
+    #     figure = corner.corner(dum,labels=[r"$\alpha_{}$".format(index),r"$\beta_{}$".format(index)],truths=[0,0])
+    #     pdf.savefig()
+    #     plt.close()
 
     # plt.plot(fit['ebeta_inv'])
     # plt.title('ebeta_inv')
