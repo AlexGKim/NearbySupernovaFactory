b91324005e8519494e4128bee97ec0e83247d1d0
Alex Kim <akim@32.51.243.131.in-addr.dhcp.lbl.gov>
2016-08-30 07:29:46 -0700

    Just about perfect

---
+++ gerard11.stan
@@ -0,0 +1,103 @@
+data {
+  int D;                // Number of supernovae
+  int N_mags;
+  int N_EWs;
+  vector[N_mags] mag_obs[D];
+  vector[N_EWs] EW_obs[D];
+  matrix[N_mags, N_mags] mag_cov[D];
+  matrix[N_EWs, N_EWs] EW_cov[D];
+  vector[D] sivel_obs;
+  vector[D] sivel_err;
+}
+
+parameters {
+  vector[5] c_raw;
+  vector[5] alpha_raw;
+  vector[5] beta_raw;
+  vector<lower=0.0>[N_mags] L_sigma_raw;
+  vector[5] eta_raw;
+
+  real<lower=0> gamma01;
+  real<lower=0> gamma02;
+  real<lower=0> gamma03;
+  real<lower=0> gamma04;
+  real<lower=0> gamma05;
+
+  real<lower=0> rho11;
+  real rho12;
+  real rho13;
+  real rho14;
+  real rho15;
+
+  real <lower=0> Delta_scale;
+  cholesky_factor_corr[N_mags] L_Omega;
+
+  vector[2] EW[D];
+  vector[D] sivel;
+  vector[N_mags] mag_int_raw[D];
+
+  simplex[D] Delta_unit;
+
+  simplex[D] k_unit;
+  simplex[D] R_unit;
+}
+
+transformed parameters {
+  vector[5] c;
+  vector[5] alpha;
+  vector[5] beta;
+  vector[5] eta;
+  vector[N_mags] L_sigma;
+
+  vector[D] Delta;
+  vector[D] k;
+  vector[D] R;
+  vector[5] gamma;
+  vector[5] rho1;
+  vector[N_mags] mag_int[D];
+
+  c = c_raw/1e2;
+  alpha = alpha_raw/5e2;
+  beta = beta_raw/2e2;
+  eta = eta_raw/2e2;
+  L_sigma = L_sigma_raw/100.;
+  
+  Delta = Delta_scale*(Delta_unit-1./D);
+  k=(k_unit-1./D);
+  R=(R_unit-1./D);
+
+  gamma[1] = gamma01;
+  gamma[2] = gamma02;
+  gamma[3] = gamma03;
+  gamma[4] = gamma04;
+  gamma[5] = gamma05;
+  gamma = gamma*5;
+
+  rho1[1] = rho11;
+  rho1[3] = rho13;
+  rho1[4] = rho14;
+  rho1[5] = rho15;
+  rho1[2] = rho12;
+  rho1 = rho1*5;
+
+    # non-centered parameterization
+  {
+    matrix[5,5] L_Sigma;
+    L_Sigma = diag_pre_multiply(L_sigma, L_Omega);
+    for (d in 1:D) {
+      mag_int[d] = Delta[d] + c+ alpha*EW[d,1]  + beta*EW[d,2]  + rho1*R[d]  + eta*sivel[d] + L_Sigma * mag_int_raw[d];
+    }
+  }
+}
+
+model {
+  target += cauchy_lpdf(L_sigma | 0.1,0.1);
+  target += lkj_corr_cholesky_lpdf(L_Omega | 4.);
+
+  for (d in 1:D) {
+    target += normal_lpdf(mag_int_raw[d]| 0, 1);
+    target += multi_normal_lpdf(mag_obs[d] | mag_int[d]+gamma*k[d], mag_cov[d]);
+    target += multi_normal_lpdf(EW_obs[d] | EW[d], EW_cov[d]);
+  }
+  target += (normal_lpdf(sivel_obs | sivel,sivel_err));
+}
---
+++ model11.py
@@ -0,0 +1,127 @@
+#!/usr/bin/env python
+
+import pickle
+import cPickle
+import numpy
+import pystan
+
+# two color parameter model
+
+pkl_file = open('gege_data.pkl', 'r')
+data = pickle.load(pkl_file)
+pkl_file.close()
+
+dic_phreno=cPickle.load(open("phrenology_2016_12_01_CABALLOv1.pkl"))
+
+dic_meta=cPickle.load(open("META.pkl"))
+
+sivel=[]
+sivel_err=[]
+for sn in data['snlist']:
+   if sn in dic_meta.keys() and sn in dic_phreno.keys():
+      meta = dic_meta[sn]
+      vSiII_6355_lbd=0.
+      vSiII_6355_lbd_err=0.
+      counter  = 0
+      for sp in dic_phreno[sn]["spectra"]:
+         if sp in meta['spectra'].keys() and  numpy.abs(meta['spectra'][sp]['salt2.phase'] < 3):
+            vSiII_6355_lbd += dic_phreno[sn]["spectra"][sp]["phrenology.vSiII_6355_lbd"]/dic_phreno[sn]['spectra'][sp]["phrenology.vSiII_6355_lbd.err"]**2
+            vSiII_6355_lbd_err += 1/dic_phreno[sn]['spectra'][sp]["phrenology.vSiII_6355_lbd.err"]**2
+            counter +=1
+      if counter !=0:
+         sivel.append(vSiII_6355_lbd / vSiII_6355_lbd_err)
+         sivel_err.append(1./numpy.sqrt(vSiII_6355_lbd_err))
+      else:
+         sivel.append(float('nan'))
+         sivel_err.append(float('nan'))
+   else:
+      sivel.append(float('nan'))
+      sivel_err.append(float('nan'))
+
+sivel = numpy.array(sivel)
+sivel_err = numpy.array(sivel_err)
+
+use = numpy.isfinite(sivel)
+
+#  The ordering is 'Ca','Si','U','B','V','R','I'
+
+EW_obs = data['obs'][:,0:2]
+mag_obs = data['obs'][:,2:]
+EW_cov = data['cov'][:,0:2,0:2]
+mag_cov = data['cov'][:,2:,2:]
+
+sivel=sivel[use]
+sivel_err = sivel_err[use]
+EW_obs=EW_obs[use]
+mag_obs=mag_obs[use]
+EW_cov= EW_cov[use]
+mag_cov=mag_cov[use]
+
+nsne, nmags = mag_obs.shape
+
+# # renormalize the data
+EW_mn = EW_obs.mean(axis=0)
+EW_renorm = (EW_obs - EW_mn)
+
+mag_mn = mag_obs.mean(axis=0)
+mag_renorm  = mag_obs-mag_mn
+
+sivel_mn = sivel.mean()
+sivel_renorm = sivel-sivel_mn
+data = {'D': nsne, 'N_mags': 5, 'N_EWs': 2, 'mag_obs': mag_renorm, 'EW_obs': EW_renorm, 'EW_cov': EW_cov, 'mag_cov':mag_cov, \
+   'sivel_obs': sivel_renorm, 'sivel_err': sivel_err}
+
+Delta_simplex = numpy.zeros(nsne-1)
+# Delta_simplex = numpy.zeros(nsne)+1./nsne
+# k_simplex = numpy.zeros(nsne)
+R_simplex = ((-1.)**numpy.arange(nsne)*.25 + .5)*2./nsne
+R_simplex = R_simplex/R_simplex.sum()
+
+init = [{'EW' : EW_renorm, \
+         'sivel': sivel_renorm,\
+         'c_raw' : numpy.zeros(5), \
+         'alpha_raw' : numpy.zeros(5), \
+         'beta_raw' : numpy.zeros(5), \
+         'eta_raw' : numpy.zeros(5), \
+         'L_sigma_raw': numpy.zeros(5)+0.03*100, \
+         # roughly the peak of one-color
+         # 'gamma01': 5.2*1.,\
+         # 'gamma03': 3.4*1.,\
+         # 'gamma03': 3.4*1.,\
+         # 'gamma04': 3.0*1.,\
+         # 'gamma05': 2.4*1.,\
+         'gamma01': 6./5,\
+         'gamma02': 4./5,\
+         'gamma03': 3./5,\
+         'gamma04': 2./5,\
+         'gamma05': 1./5,\
+         'mag_int_raw': mag_renorm, \
+         'L_Omega': numpy.identity(5), \
+         'Delta_unit':R_simplex, \
+         'Delta_scale': 2.5, \
+         'k_unit': R_simplex, \
+         # 'k_scale': 20, \
+         'R_unit': R_simplex, \
+         # 'R_scale': 20, \
+         # 'rho11': 4.4/100.,\
+         # 'rho13': 2.6/100.,\
+         # 'rho14': 2.2/100.,\
+         # 'rho15': 1.8/100.,\
+         'rho11': 17./5,\
+         'rho12': 0.*3./5,\
+         'rho13': 0./5,\
+         'rho14': 0.*3./5,\
+         'rho15': 0.*3./5,\
+         # 'rho1':numpy.zeros(5)\
+         } \
+        for _ in range(4)]
+
+sm = pystan.StanModel(file='gerard11.stan')
+control = {'stepsize':1}
+fit = sm.sampling(data=data, iter=2000, chains=4,control=control,init=init, thin=1)
+
+
+output = open('temp11.pkl','wb')
+pickle.dump((fit.extract(),fit.get_sampler_params()), output, protocol=2)
+output.close()
+print fit
