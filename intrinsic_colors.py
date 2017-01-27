bee51b01ebc584528f5de3fecdc6302a2aab4201
Alex Kim <akim@251.51.243.131.in-addr.dhcp.lbl.gov>
2017-01-26 16:23:40 -0800

    compare salt2 and Hsiao

---
+++ intrinsic_colors.py
@@ -0,0 +1,178 @@
+#!/usr/bin/env python
+
+import pickle
+import pystan
+import matplotlib.pyplot as plt
+from matplotlib import rc
+import corner
+from matplotlib.backends.backend_pdf import PdfPages
+import numpy
+import sncosmo
+import scipy
+import cPickle
+import matplotlib as mpl
+import sivel
+
+ext=''
+
+f = open('temp18'+ext+'.pkl','rb')
+(fit18,_) = pickle.load(f)
+
+
+
+f = open('temp11'+ext+'.pkl','rb')
+(fit11,_) = pickle.load(f)
+
+# for key in fit.keys():
+#     print key, fit[key].min(), fit[key].max()
+
+pkl_file = open('gege_data.pkl', 'r')
+data = pickle.load(pkl_file)
+pkl_file.close()
+
+sivel,sivel_err,x1,x1_err, _, _ = sivel.sivel(data)
+use = numpy.isfinite(sivel)
+
+#  The ordering is 'Ca','Si','U','B','V','R','I'
+
+EW_obs = data['obs'][:,0:2]
+mag_obs = data['obs'][:,2:]
+EW_cov = data['cov'][:,0:2,0:2]
+mag_cov = data['cov'][:,2:,2:]
+
+sivel=sivel[use]
+sivel_err = sivel_err[use]
+x1=x1[use]
+x1_err = x1_err[use]
+EW_obs=EW_obs[use]
+mag_obs=mag_obs[use]
+EW_cov= EW_cov[use]
+mag_cov=mag_cov[use]
+
+EW_mn = EW_obs.mean(axis=0)
+EW_renorm = (EW_obs - EW_mn)
+
+mag_mn = mag_obs.mean(axis=0)
+mag_renorm  = mag_obs-mag_mn
+
+sivel_mn = sivel.mean()
+sivel_renorm = sivel-sivel_mn
+
+nsne, nmags = mag_obs.shape
+color_obs = numpy.zeros((nsne,nmags-1))
+color_obs[:,0] = mag_renorm[:,0]- mag_renorm[:,2]
+color_obs[:,1] = mag_renorm[:,1]- mag_renorm[:,2]
+color_obs[:,2] = mag_renorm[:,3]- mag_renorm[:,2]
+color_obs[:,3] = mag_renorm[:,4]- mag_renorm[:,2]
+
+pkl_file.close()
+
+trans = [[1.,0,-1,0,0],[0.,1,-1,0,0],[0.,0,1,-1,0],[0.,0,1,0,-1]]
+trans = numpy.array(trans)
+color_cov = numpy.zeros((nsne,4,4))
+for i in xrange(nsne):
+    color_cov[i] = numpy.dot(trans,numpy.dot(mag_cov[i], trans.T))
+
+colors = fit18['c']+mag_mn[None,:]
+colors  = colors - colors[:,2][:,None]
+colors18 = numpy.delete(colors, 2,axis=1)
+
+
+colors = fit11['c']+mag_mn[None,:]
+colors  = colors - colors[:,2][:,None]
+colors11 = numpy.delete(colors, 2,axis=1)
+
+
+from matplotlib.ticker import FuncFormatter, MaxNLocator
+def format_fn(tick_val, tick_pos):
+    if int(tick_val) in numpy.arange(4):
+        return labels[int(tick_val)]
+    else:
+        return ''
+
+colors18_perc = numpy.percentile(colors18,(50,50-34,50+34),axis=0)
+colors11_perc = numpy.percentile(colors11,(50,50-34,50+34),axis=0)
+print colors18_perc[0], colors11_perc[0]
+ecolor = colors11_perc[0] - colors18_perc[0]
+decolor = numpy.sqrt((colors18_perc[2]-colors18_perc[1])**2/4 + (colors11_perc[2]-colors11_perc[1])**2/4)
+fig = plt.figure()
+ax = fig.add_subplot(111)
+labels = list('UBRI')
+ax.errorbar(numpy.arange(4),ecolor,yerr=decolor,label='Model Difference')
+ax.xaxis.set_major_formatter(FuncFormatter(format_fn))
+ax.xaxis.set_major_locator(MaxNLocator(integer=True))
+ax.set_xlim((-0.5,3.5))
+
+
+snmod='salt2'
+
+synlam = numpy.array([[3300.00, 3978.02]
+    ,[3978.02,4795.35]
+    ,[4795.35,5780.60]
+    ,[5780.60,6968.29]
+    ,[6968.29,8400.00]])
+
+synname=['U','B','V','R','I']
+
+synbands=[]
+
+for name, lams in zip(synname,synlam):
+    synbands.append(sncosmo.Bandpass(lams, [1.,1.], name='tophat'+name))
+
+model_nodust = sncosmo.Model(source=snmod)
+flux_nodust = model_nodust.bandflux(synbands,0.)
+rv=numpy.arange(2,4,.5)
+for r in rv:
+    dust = sncosmo.F99Dust(r_v=r)
+    dust.set(ebv=0.3)
+    model = sncosmo.Model(source=snmod, effects=[dust], effect_names=['host'], effect_frames=['rest'])
+    AX=-2.5*numpy.log10(model.bandflux(synbands,0.)/flux_nodust)
+    AX  = AX - AX[2]
+    AX = numpy.delete(AX, 2)
+    ax.plot(numpy.arange(4),AX,label=r'$R={}$'.format(r))
+    ax.set_ylabel(r'$E(X-V)$')
+    ax.set_xlabel(r'Band $X$')
+plt.legend()
+pp = PdfPages('output18'+ext+'/model1118ebv.pdf')
+plt.savefig(pp,format='pdf')
+pp.close()
+plt.close()
+
+
+labels = list('UBVRI')
+def format_fn2(tick_val, tick_pos):
+    if int(tick_val) in numpy.arange(5):
+        return labels[int(tick_val)]
+    else:
+        return ''
+
+
+rs = numpy.percentile((fit11['c'][:16000,:]-fit18['c'])/(colors11[:16000,1]-colors18[:,1])[:,None],(50,50-34,50+34),axis=0)
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.errorbar(numpy.arange(5), rs[0], yerr=[rs[0]-rs[1],rs[2]-rs[0]],fmt='o')
+ax.xaxis.set_major_formatter(FuncFormatter(format_fn2))
+ax.xaxis.set_major_locator(MaxNLocator(integer=True))
+ax.set_xlim((-0.5,4.5))
+ax.set_xlabel(r'Band $X$')
+ax.set_ylabel(r'$A_X / E(B-V)$ of F99 relative to Linear Model')
+pp = PdfPages('output18'+ext+'/comptolinear.pdf')
+plt.savefig(pp,format='pdf')
+pp.close()
+plt.close()
+
+wefwe
+c11mn = numpy.median(colors11,axis=0)
+c18mn = numpy.median(colors18,axis=0)
+print c11mn-c18mn
+
+wef
+
+
+figure = corner.corner(colors,labels=[r"$U_0-V_0$",r"$B_0-V_0$",r"$R_0-V_0$",r"$I_0-V_0$"])
+pp = PdfPages('output18'+ext+'/col_corner.pdf')
+plt.savefig(pp,format='pdf')
+pp.close()
+plt.close()
+
+
---
+++ viewfitz.py
@@ -0,0 +1,177 @@
+#!/usr/bin/env python
+
+import pickle
+import pystan
+import matplotlib.pyplot as plt
+from matplotlib import rc
+import corner
+from matplotlib.backends.backend_pdf import PdfPages
+import numpy
+import sncosmo
+import scipy
+import cPickle
+import matplotlib as mpl
+import sivel
+
+ext=''
+
+f = open('temp18'+ext+'.pkl','rb')
+(fit18,_) = pickle.load(f)
+f = open('temp18'+ext+'_free.pkl','rb')
+(fit18_free,_) = pickle.load(f)
+f = open('temp18'+ext+'_salt2.pkl','rb')
+(fit18_salt2,_) = pickle.load(f)
+f = open('temp19'+ext+'.pkl','rb')
+(fit19,_) = pickle.load(f)
+f = open('temp20'+ext+'.pkl','rb')
+(fit20,_) = pickle.load(f)
+f = open('temp20'+ext+'_salt2.pkl','rb')
+(fit20_salt2,_) = pickle.load(f)
+f = open('temp20_ccm.pkl','rb')
+(fit20_ccm,_) = pickle.load(f)
+f = open('temp11'+ext+'.pkl','rb')
+(fit11,_) = pickle.load(f)
+
+
+
+
+plt.hist(fit18_salt2['AV'].flatten(),histtype='step', stacked='false', \
+    label=r'$R=const$ SALT2',bins=numpy.arange(-.25,1.8,0.1),normed=True)
+plt.hist(fit18['AV'].flatten(),histtype='step', stacked='false', \
+    label=r'$R=const$ Hsiao',bins=numpy.arange(-.25,1.8,0.1),normed=True)
+# plt.hist(fit18_free['AV'].flatten(),histtype='step', stacked='false', \
+#     label=r'$R=const$ free',bins=numpy.arange(-.25,1.8,0.1),normed=True)
+# plt.hist(fit19['AV'].flatten(),histtype='step', stacked='false', \
+#     label=r'$\ln{R}\sim \mathcal{N}$',bins=numpy.arange(-.25,1.8,0.1),normed=True)
+# plt.hist(fit20['AV'].flatten(),histtype='step', stacked='false', \
+#     label=r'$R$-free',bins=numpy.arange(-.25,1.8,0.1),normed=True)
+# plt.hist(fit20_salt2['AV'].flatten(),histtype='step', stacked='false', \
+#     label=r'$R$-free S2',bins=numpy.arange(-.25,1.8,0.1),normed=True)
+
+plt.xlabel(r'$A_V$')
+plt.legend()
+pp = PdfPages('output18'+ext+'/AVs_hist.pdf')
+plt.savefig(pp,format='pdf')
+pp.close()
+plt.close()
+
+plt.hist(numpy.median(fit18_salt2['AV'],axis=0),label=r'$R=const$ SALT2',histtype='step',bins=numpy.arange(-0.2,1.8,0.1),normed=True)
+plt.hist(numpy.median(fit18['AV'],axis=0),label=r'$R=const$ Hsiao',histtype='step',bins=numpy.arange(-0.2,1.8,0.1),normed=True)
+# plt.hist(numpy.median(fit19['AV'],axis=0),label=r'$\ln{R}\sim \mathcal{N}$',histtype='step',bins=numpy.arange(-0.2,1.8,0.1),normed=True)
+# plt.hist(numpy.median(fit20['AV'],axis=0),label=r'$R$-free',histtype='step',bins=numpy.arange(-0.2,1.8,0.1),normed=True)
+# plt.hist(numpy.median(fit20_salt2['AV'],axis=0),label=r'$R$-free S2',histtype='step',bins=numpy.arange(-0.2,1.8,0.1),normed=True)
+# plt.hist(numpy.median(fit20_ccm['AV'],axis=0),label=r'$R$-free CCM',histtype='step',bins=numpy.arange(-0.2,1.8,0.1))
+plt.hist(numpy.median(fit11['gamma'][:,2][:,None]*fit11['k'],axis=0),label=r'$\gamma_0 k_0$',histtype='step',bins=numpy.arange(-0.2,1.8,0.1),normed=True)
+plt.xlabel(r'$A_V$')
+plt.legend()
+pp = PdfPages('output18'+ext+'/AVs_mode_hist.pdf')
+plt.savefig(pp,format='pdf')
+pp.close()
+plt.close()
+
+# plt.hist(numpy.median(fit20_ccm['AV']-fit20['AV'],axis=0),histtype='step')
+# # plt.hist(numpy.median(fit20_ccm['AV'],axis=0),label=r'$R$-free CCM',histtype='step',bins=numpy.arange(-0.2,1.8,0.1))
+# plt.xlabel(r'$A_V(CCM) - A_V(F99)$')
+# pp = PdfPages('output18'+ext+'/AVs_mode_hist_comp.pdf')
+# plt.savefig(pp,format='pdf')
+# pp.close()
+# plt.close()
+
+
+(ymin, ymax) = numpy.percentile(fit18['AV'],(50-34,50+34),axis=0)
+f18perc=(ymax-ymin)/2
+(ymin, ymax) = numpy.percentile(fit19['AV'],(50-34,50+34),axis=0)
+f19perc=(ymax-ymin)/2
+(ymin, ymax) = numpy.percentile(fit20['AV'],(50-34,50+34),axis=0)
+f20perc=(ymax-ymin)/2
+(ymin, ymax) = numpy.percentile(fit20_ccm['AV'],(50-34,50+34),axis=0)
+f20_ccmperc=(ymax-ymin)/2
+(ymin, ymax) = numpy.percentile(fit11['gamma'][:,2][:,None]*fit11['k'],(50-34,50+34),axis=0)
+f11perc=(ymax-ymin)/2
+
+print "{:6.2f} {:6.2f} {:6.2f} {:6.2f}".format(f18perc.mean(),f19perc.mean(),f20perc.mean(),f20_ccmperc.mean(),f11perc.mean())
+
+
+n=10000
+choice = numpy.random.randint(0,len(fit19['lnRV_mn']),size=n)
+raw = numpy.random.normal(0,1,size=n)
+fit19RV = numpy.exp(fit19['lnRV_mn'][choice] + raw*fit19['lnRV_sig'][choice])
+shit=numpy.percentile(1./fit18_salt2['RVinv'],(50,50-34,50+34))
+print shit[0],shit[0]-shit[1],shit[2]-shit[0]
+shit= numpy.percentile(1./fit18['RVinv'],(50,50-34,50+34))
+print shit[0],shit[0]-shit[1],shit[2]-shit[0]
+plt.hist(1./fit18_salt2['RVinv'].flatten(),histtype='step', stacked='false', \
+    label=r'$R=const$ SALT2',bins=numpy.arange(2,4.2,0.05),normed=True)
+plt.hist(1./fit18['RVinv'].flatten(),histtype='step', stacked='false', \
+    label=r'$R=const$ Hsiao',bins=numpy.arange(2,4.2,0.05),normed=True)
+# plt.hist(1./fit18_free['RVinv'].flatten(),histtype='step', stacked='false', \
+#     label=r'$R=const$ free',bins=numpy.arange(0,8.2,0.1),normed=True)
+# plt.hist(fit19RV,histtype='step', stacked='false', \
+#     label=r'$\ln{R}\sim \mathcal{N}$',bins=numpy.arange(0,8.2,0.1),normed=True)
+# plt.hist(numpy.median(fit20['RV'],axis=0),histtype='step', stacked='false', \
+#     label=r'$R$-free',bins=numpy.arange(0,8.2,0.25),normed=True)
+# plt.hist(numpy.median(fit20_salt2['RV'],axis=0),histtype='step', stacked='false', \
+#     label=r'$R$-free S2',bins=numpy.arange(0,8.2,0.25),normed=True)
+# plt.hist(numpy.median(fit20_ccm['RV'],axis=0),histtype='step', stacked='false', \
+#     label=r'$R$-free CCM',bins=numpy.arange(0,8.2,0.1),normed=True)
+plt.xlabel(r'$R_V$')
+plt.legend()
+pp = PdfPages('output18'+ext+'/RVs_hist.pdf')
+plt.savefig(pp,format='pdf')
+pp.close()
+plt.close()
+
+
+
+# plt.hist(numpy.median(fit20_ccm['RV']-fit20['RV'],axis=0),histtype='step', stacked='false',normed=True,color='red')
+# plt.xlabel(r'$R_V(CCM)-R_V(F99)$')
+# pp = PdfPages('output18'+ext+'/RVs_hist_comp.pdf')
+# plt.savefig(pp,format='pdf')
+# pp.close()
+# plt.close()
+
+# plt.hist(numpy.median(fit20_ccm['AV']-fit20['AV'],axis=0),histtype='step')
+# # plt.hist(numpy.median(fit20_ccm['AV'],axis=0),label=r'$R$-free CCM',histtype='step',bins=numpy.arange(-0.2,1.8,0.1))
+# plt.xlabel(r'$A_V(CCM) - A_V(F99)$')
+# pp = PdfPages('output18'+ext+'/AVs_mode_hist_comp.pdf')
+# plt.savefig(pp,format='pdf')
+# pp.close()
+# plt.close()
+
+
+(y, ymin, ymax) = numpy.percentile(fit20['RV'],(50,50-34,50+34),axis=0)
+(x, xmin, xmax) = numpy.percentile(fit20['AV'],(50,50-34,50+34),axis=0)
+
+
+plt.errorbar(x, y, xerr=[x-xmin,xmax-xmin],yerr=[y-ymin,ymax-y],fmt='.',alpha=0.15,color='b')
+plt.scatter(x, y,marker='o',alpha=0.3,s=4,c='b')
+# plt.xlim((-0.5,0.75))
+# plt.ylim((-4,6))
+plt.xlabel(r'$A_V^F$')
+plt.ylabel(r'$R_V^F$')
+pp = PdfPages('output18'+ext+'/AVRV.pdf')
+plt.savefig(pp,format='pdf')
+pp.close()
+plt.close()
+
+# (y, ymin, ymax) = numpy.percentile(fit20_ccm['RV']-fit20['RV'],(50,50-34,50+34),axis=0)
+# (x, xmin, xmax) = numpy.percentile(fit20_ccm['AV']-fit20['AV'],(50,50-34,50+34),axis=0)
+
+# dx = (xmax-xmin)/2
+# print numpy.sum(x/dx**2)/numpy.sum(1/dx**2)
+# print numpy.sqrt(1/numpy.sum(1/dx**2))
+
+# dy = (ymax-ymin)/2
+# print numpy.sum(y/dy**2)/numpy.sum(1/dy**2)
+# print numpy.sqrt(1/numpy.sum(1/dy**2))
+
+# plt.errorbar(x, y, xerr=[x-xmin,xmax-xmin],yerr=[y-ymin,ymax-ymin],fmt='.',alpha=0.15,color='b')
+# plt.scatter(x, y,marker='o',alpha=0.3,s=4,c='b')
+# plt.xlim((-0.5,0.75))
+# plt.ylim((-4,6))
+# plt.xlabel(r'$A_V^C - A_V^F$')
+# plt.ylabel(r'$R_V^C-R_V^F$')
+# pp = PdfPages('output18'+ext+'/CCMF99.pdf')
+# plt.savefig(pp,format='pdf')
+# pp.close()
+# plt.close()
