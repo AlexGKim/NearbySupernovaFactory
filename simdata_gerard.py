3e60bc51c43df8573c96cad4f74e2f71a0503e9f
Alex Kim <akim@tibia.local>
2017-03-25 14:30:08 -0700

    fix errors in model and improve distribution

--- simdata_gerard.py
+++ simdata_gerard.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 
+# perfect standard candle
+
 import pickle
 import cPickle
 import numpy
 import pystan
-import sivel
+import sivel as sivel2
 import sncosmo
-
+print sncosmo.__version__
 # two color parameter model
 numpy.random.seed(seed=1)
 
@@ -28,8 +30,12 @@ pkl_file = open('gege_data.pkl', 'r')
 data = pickle.load(pkl_file)
 pkl_file.close()
 
-sivel, sivel_err,_,_ = sivel.sivel(data)
 
+f = open('temp11.pkl','rb')
+(fit,_) = pickle.load(f)
+f.close()
+
+sivel,sivel_err,x1,x1_err,zcmb,zerr = sivel2.sivel(data)
 
 use = numpy.isfinite(sivel)
 
@@ -47,7 +53,65 @@ mag_obs=mag_obs[use]
 EW_cov= EW_cov[use]
 mag_cov=mag_cov[use]
 
+#The values to use
+
+EW = numpy.median(fit['EW'],axis=0)
+sivel = numpy.median(fit['sivel'],axis=0)
+
+
 nsne, nmags = mag_obs.shape
+
+
+numpy.random.seed(seed=0)
+
+
+intrinsic=[]
+for i in xrange(nsne):
+    intrinsic.append(numpy.zeros(5)+numpy.random.normal(0,0.08))
+intrinsic = numpy.array(intrinsic)
+
+av = fit['gamma'][:,2][:,None]*fit['k'] + fit['rho1'][:,2][:,None]*fit['R']
+av=numpy.median(av,axis=0)
+# ab = fit['gamma'][:,1][:,None]*fit['k'] + fit['rho1'][:,1][:,None]*fit['R']
+# ab=numpy.median(ab,axis=0)
+
+ebv = (fit['gamma'][:,1]-fit['gamma'][:,2])[:,None]*fit['k'] + \
+    (fit['rho1'][:,1]-fit['rho1'][:,2])[:,None]*fit['R']
+ebv=numpy.median(ebv,axis=0)
+ebv=ebv-ebv.min()
+
+# wef
+# import matplotlib.pyplot as plt
+# plt.hist(ebv-ebv.min())
+# plt.show()
+
+rv = numpy.random.lognormal(numpy.log(2.4),0.05, size=nsne)
+# import matplotlib.pyplot as plt
+# plt.scatter(rv, rv*ebv)
+# plt.show()
+# wfew
+mag_obs=[]
+EW_obs=[]
+sivel_obs=[]
+for i in xrange(nsne):
+    dust = sncosmo.F99Dust(r_v=rv[i])
+    dust.set(ebv=ebv[i])
+    # model0 = sncosmo.Model(source='hsiao')
+    # model0.parameters[2]=x0
+    # model0.parameters[3]=x1
+    # model0.parameters[4]=c
+    model = sncosmo.Model(source='hsiao', effects=[dust], effect_names=['host'], effect_frames=['rest'])
+    # model.parameters[2]=x0
+    # model.parameters[3]=x1
+    # model.parameters[4]=c
+    # mag_obs.append(numpy.random.multivariate_normal(intrinsic[i,:]+2.5*numpy.log10(model0.bandflux(synbands,0.)/model.bandflux(synbands,0.)),mag_cov[i]))
+    mag_obs.append(numpy.random.multivariate_normal(intrinsic[i,:]+model.bandmag(synbands,'vega',0.),mag_cov[i]))
+    EW_obs.append(numpy.random.multivariate_normal(EW[i,:],EW_cov[i]))
+    sivel_obs.append(numpy.random.normal(sivel[i],sivel_err[i]))
+
+mag_obs=numpy.array(mag_obs)
+EW_obs=numpy.array(EW_obs)
+sivel_obs = numpy.array(sivel_obs)
 
 # # renormalize the data
 EW_mn = EW_obs.mean(axis=0)
@@ -56,65 +120,17 @@ EW_renorm = (EW_obs - EW_mn)
 mag_mn = mag_obs.mean(axis=0)
 mag_renorm  = mag_obs-mag_mn
 
+# import matplotlib.pyplot as plt
+# plt.scatter(mag_renorm[:,1]-mag_renorm[:,2],mag_renorm[:,2])
+# plt.show()
+
 sivel_mn = sivel.mean()
 sivel_renorm = sivel-sivel_mn
 
-# Truth
-
-Delta = numpy.random.uniform(low=-0.2, high=0.2, size=nsne)
-c = numpy.zeros(5)
-alpha = numpy.array([0.005, 0.002,0.002,0.002,0.003])
-beta = numpy.array([0.03, 0.03,0.03,0.02,0.02])
-eta = numpy.array([-0.0002,0,0.0005,0.0005,-0.0003])
-delta = numpy.array([-0.737, 0.129,1.961,0.598,-2.545])
-av = numpy.random.exponential(scale=0.1, size=nsne)
-rv = numpy.random.lognormal(mean=0.916, sigma=0.1, size=nsne)
-
-cov =numpy.array([[0.0038, 0.001,-0.0002,0,0.0003],[0.001,0.0011,0.0002,0.,-0.0006],[-0.0002,0.0002,0.0004,0,-0.0002],\
-   [0,0,0,0.0002,0.0002],[0.0003,-0.0006,-0.0002,0.0002,0.002]])
-
-
-EW = numpy.array(EW_renorm)
-sivel = numpy.array(sivel_renorm)
-D = numpy.random.uniform(low=0.02, high=0.02, size=nsne)
-
-mn = Delta[:,None] + alpha[None,:] * EW[:,0][:,None] + \
-   beta[None,:] * EW[:,1][:,None] + eta[None,:]*sivel[:,None] + delta[None,:]*D[:,None]
-
-intrinsic=[]
-for i in xrange(nsne):
-    intrinsic.append(numpy.random.multivariate_normal(mn[i,:],cov))
-intrinsic = numpy.array(intrinsic)
-
-mag_obs=[]
-EW_obs=[]
-sivel_obs=[]
-for i in xrange(nsne):
-    dust = sncosmo.F99Dust(r_v=rv[i])
-    dust.set(ebv=av[i]/rv[i])
-    x0 = numpy.random.lognormal(mean=0, sigma=0.05)
-    x1 = numpy.random.lognormal(mean=0, sigma=0.2)
-    c = numpy.random.lognormal(mean=0, sigma=0.02)
-    model0 = sncosmo.Model(source='salt2')
-    model0.parameters[2]=x0
-    model0.parameters[3]=x1
-    model0.parameters[4]=c
-    model = sncosmo.Model(source='salt2', effects=[dust], effect_names=['host'], effect_frames=['rest'])
-    model.parameters[2]=x0
-    model.parameters[3]=x1
-    model.parameters[4]=c
-    mag_obs.append(numpy.random.multivariate_normal(intrinsic[i,:]+2.5*numpy.log10(model0.bandflux(synbands,0.)/model.bandflux(synbands,0.)),mag_cov[i]))
-    EW_obs.append(numpy.random.multivariate_normal(EW[i,:],EW_cov[i]))
-    sivel_obs.append(numpy.random.normal(sivel[i],sivel_err[i]))
-
-mag_obs=numpy.array(mag_obs)
-EW_obs=numpy.array(EW_obs)
-sivel_obs = numpy.array(sivel_obs)
-
-data = {'D': nsne, 'N_mags': 5, 'N_EWs': 2, 'mag_obs': mag_obs, 'EW_obs': EW_obs, 'EW_cov': EW_cov, 'mag_cov':mag_cov, \
-   'sivel_obs': sivel_obs, 'sivel_err': sivel_err}
+data = {'D': nsne, 'N_mags': 5, 'N_EWs': 2, 'mag_obs': mag_renorm, 'EW_obs': EW_renorm, 'EW_cov': EW_cov, 'mag_cov':mag_cov, \
+   'sivel_obs': sivel_renorm, 'sivel_err': sivel_err}
 
 
 output = open('simdata_gerard.pkl','wb')
-pickle.dump([data, intrinsic, av, rv, D, Delta,alpha,beta,eta,delta], output, protocol=2)
+pickle.dump(data, output, protocol=2)
 output.close()
