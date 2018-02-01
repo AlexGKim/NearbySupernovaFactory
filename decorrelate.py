8ffb895f4959ffa119fe8c035e4f64de1d4c7d16
Alex Kim <akim@69.201.128.198.in-addr.dhcp.lbnl.us>
2018-01-31 16:16:59 -0800

    incororated decorrelation of g's with Delta

--- decorrelate.py
+++ decorrelate.py
@@ -11,6 +11,7 @@ def fix3_x1():
     outfit= copy.deepcopy(fit)
 
     for i in xrange(fit['x1'].shape[0]):
+    # for i in xrange(1):
         # for j in xrange(fit['Delta'].shape[1]):
         #     print "original"
         #     print fit['c'][i,:]+fit['alpha'][i,:]*fit['EW'][i,j,0]+fit['beta'][i,:]* fit['EW'][i,j,1] +\
@@ -24,16 +25,20 @@ def fix3_x1():
         #         fit['eta'][i,:]* fit['sivel'][i,j] + fit['zeta'][i,:]* fit['x1'][i,j] +\
         #         fit['gamma'][i,:] * newk  + fit['rho1'][i,:]* fit['R'][i,j]
 
-        hs = numpy.array([fit['EW'][i,:,0],fit['EW'][i,:,1],fit['sivel'][i,:], fit['x1'][i,:],fit['Delta'][i,:]])
+        hs = numpy.array([fit['EW'][i,:,0],fit['EW'][i,:,1],fit['sivel'][i,:], fit['x1'][i,:], \
+            fit['k'][i,:], fit['R'][i,:], fit['ev_sig'][i]*fit['mag_int_raw'][i,:],fit['Delta'][i,:]])
         bigcov = numpy.cov(hs)
-        hscov = bigcov[:4,:4]
-        hdcov = bigcov[:4,4]
+        hscov = bigcov[:7,:7]
+        hdcov = bigcov[:7,7]
         c = numpy.dot(numpy.linalg.inv(hscov), hdcov)
 
         newalpha = fit['alpha'][i,:] + c[0]
         newbeta = fit['beta'][i,:] + c[1]
         neweta = fit['eta'][i,:] + c[2]
         newzeta = fit['zeta'][i,:] + c[3]
+        newgamma = fit['gamma'][i,:] + c[4]
+        newrho1 = fit['rho1'][i,:] + c[5]
+        newev = fit['ev'][i,:] + c[6]
 
         newDelta=[]
         for j in xrange(fit['Delta'].shape[1]):
@@ -43,7 +48,7 @@ def fix3_x1():
             #     fit['eta'][i,:]* fit['sivel'][i,j] + fit['zeta'][i,:]* fit['x1'][i,j]
 
             newDelta.append(fit['Delta'][i,j] - \
-                numpy.dot(c,numpy.array([fit['EW'][i,j,0],fit['EW'][i,j,1],fit['sivel'][i,j], fit['x1'][i,j]])))
+                numpy.dot(c,numpy.array([fit['EW'][i,j,0],fit['EW'][i,j,1],fit['sivel'][i,j], fit['x1'][i,j], fit['k'][i,j], fit['R'][i,j], fit['ev_sig'][i]* fit['mag_int_raw'][i,j]])))
 
             # print "new" 
             # print newDelta + newalpha*fit['EW'][i,j,0]+newbeta* fit['EW'][i,j,1] +\
@@ -52,8 +57,8 @@ def fix3_x1():
         newDelta = numpy.array(newDelta)
 
         # # now do latent parameters
-        latents =[fit['k'][i,:],fit['R'][i,:],fit['mag_int_raw'][i,:]]
-        cofactors =[fit['gamma'][i,:],fit['rho1'][i,:],fit['ev_sig'][i] * fit['ev'][i,:]]
+        latents =[fit['k'][i,:],fit['R'][i,:],fit['ev_sig'][i]*fit['mag_int_raw'][i,:]]
+        cofactors =[newgamma,newrho1,newev]
         newpars = []
         for lat, cof in zip(latents,cofactors):
             hs = numpy.array([fit['EW'][i,:,0],fit['EW'][i,:,1],fit['sivel'][i,:], fit['x1'][i,:],lat])
@@ -91,9 +96,12 @@ def fix3_x1():
         outfit['eta'][i,:]  = neweta
         outfit['zeta'][i,:] = newzeta
         outfit['Delta'][i,:] = newDelta
+        outfit['gamma'][i,:] = newgamma
+        outfit['rho1'][i,:] = newrho1
+        outfit['ev'][i,:] = newev
         outfit['k'][i,:] = newpars[0]
         outfit['R'][i,:] = newpars[1]
-        outfit['mag_int_raw'][i,:]  = newpars[2]
+        outfit['mag_int_raw'][i,:]  = newpars[2]/fit['ev_sig'][i]
 
     # for i in xrange(1):#fit['x1'].shape[0]):
     #     for j in xrange(fit['Delta'].shape[1]):
@@ -103,7 +111,7 @@ def fix3_x1():
     #             fit['rho1'][i,:]*fit['R'][i,j] +fit['ev_sig'][i] * fit['ev'][i,:]* fit['mag_int_raw'][i,j]
     #         term2 = outfit['Delta'][i,j] + outfit['alpha'][i,:]*outfit['EW'][i,j,0]+outfit['beta'][i,:]* outfit['EW'][i,j,1] +\
     #             outfit['eta'][i,:]* outfit['sivel'][i,j] + outfit['zeta'][i,:]* outfit['x1'][i,j] + outfit['gamma'][i,:]*outfit['k'][i,j]+\
-    #             outfit['rho1'][i,:]*outfit['R'][i,j] +outfit['ev_sig'][i] * outfit['ev'][i,:]* outfit['mag_int_raw'][i,j]
+    #             outfit['rho1'][i,:]*outfit['R'][i,j] +outfit['ev_sig'][i]* outfit['ev'][i,:]* outfit['mag_int_raw'][i,j]
     #         print term1-term2
     #     hs = numpy.array([outfit['EW'][i,:,0],outfit['EW'][i,:,1],outfit['sivel'][i,:], outfit['x1'][i,:],outfit['Delta'][i,:], \
     #         outfit['k'][i,:],outfit['R'][i,:],outfit['mag_int_raw'][i,:]])
@@ -325,4 +333,4 @@ def fix1():
     pickle.dump(outfit, output, protocol=2)
     output.close()
 
-fix1()
+fix3_x1()
